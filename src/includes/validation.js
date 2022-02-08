17b3bcaf9434784fa55e96a1aeae1a2bed4b90a2
ohmnapt <napats.work@gmail.com>
2022-02-09 02:10:29 +0700

    setting up the login form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -49,6 +49,10 @@ export default {
 
         return message;
       },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
     });
   },
 };
